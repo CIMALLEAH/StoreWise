7b85d0d4d3ba5b4df412b3c800aa4bfa5fa0714d
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-12-01 20:10:27 +0800

    Add files via upload

--- StoreWise_OOP/MainMenu.java
+++ StoreWise_OOP/MainMenu.java
@@ -13,16 +13,11 @@ public class MainMenu {
 
     
     public static void main(String[] args) {
-        Utils.clearConsole();
-        Utils.printLine(60);
-        Utils.printCentered("StoreWise");
-        Utils.printLine(60);
-        Utils.printCentered("Welcome to StoreWise");
-        Utils.sleepFor(1500);
+        Utils.displayHeader("StoreWise");
+        Utils.displayMessage("Welcome to StoreWise!!");
         boolean running = true;
 
         while (running) {
-            // Show Main Menu
             if (!userManager.isLoggedIn()) {
                 showMainMenu();
             } else {
@@ -30,16 +25,13 @@ public class MainMenu {
             }
         }
 
-        scanner.close();  // Close the scanner at the end of the program
+        scanner.close();
     }
 
     private static void showMainMenu() {
         int choice = 0;
         while (true) {
-            Utils.clearConsole();
-            Utils.printLine(60);
-            Utils.printCentered(" Main Menu");
-            Utils.printLine(60);
+            Utils.displayHeader("Main Menu");
             System.out.println("  1. Login");
             System.out.println("  2. Exit");
             Utils.printLine(60);
@@ -47,130 +39,86 @@ public class MainMenu {
             
             if (scanner.hasNextInt()) {
                 choice = scanner.nextInt();
-                scanner.nextLine();  // Consume newline character
+                scanner.nextLine();  
                 if (choice == 1 || choice == 2) {
-                    break;  // Exit the loop if input is valid
+                    break;
                 } else {
-                    Utils.clearConsole();
-                    Utils.printLine(60);
-                    Utils.printCentered("Main Menu");
-                    Utils.printLine(60);
-                    Utils.printCentered("Invalid input, please enter 1 or 2.");
-                    Utils.sleepFor(2000);
-                    Utils.clearConsole();
+                    Utils.displayHeader("Main Menu");
+                    Utils.displayMessage("Invalid input, please enter 1 or 2.");
                 }
             } else {
-                Utils.clearConsole();
-                Utils.printLine(60);
-                Utils.printCentered("Main Menu");
-                Utils.printLine(60);
-                Utils.printCentered("Invalid input, please enter a number.");
-                Utils.sleepFor(2000);
-                Utils.clearConsole();
-                scanner.nextLine();  // Consume invalid input
+                Utils.displayHeader("Main Menu");
+                Utils.displayMessage("Invalid input, please enter a number.");
+                scanner.nextLine(); 
             }
         }
 
         if (choice == 1) {
-            // Login process
             boolean loginSuccess = false;
             while (!loginSuccess) {
-                Utils.clearConsole();
-                Utils.printLine(60);
-                Utils.printCentered("Login");
-                Utils.printLine(60);
+                Utils.displayHeader("Login");
                 System.out.print(" Enter username: ");
                 String username = scanner.nextLine();
                 System.out.print(" Enter password: ");
                 String password = scanner.nextLine();
 
                 if (userManager.login(username, password)) {
-                    Utils.clearConsole();
-                    Utils.printLine(60);
-                    Utils.printCentered("Login");
-                    Utils.printLine(60);
-                    Utils.printCentered("Login successful!");
-                    Utils.sleepFor(2000);
-                    Utils.clearConsole(); // Clear console after login
+                    Utils.displayHeader("Login");
+                    Utils.displayMessage("Login successful!");
                     loginSuccess = true;
                 } else {
-                    Utils.clearConsole();
-                    Utils.printLine(60);
-                    Utils.printCentered("Login");
-                    Utils.printLine(60);
-                    Utils.printCentered("Invalid username or password.");
-                    Utils.sleepFor(1000);
-                    Utils.clearConsole();
+                    Utils.displayHeader("Login");
+                    Utils.displayMessage("Invalid username or password.");
 
-                    // Loop until a valid input ('y' or 'n') is entered
                     while (true) {
-                        // Ask the user if they want to try again or exit
-                        Utils.printLine(60);
-                        Utils.printCentered("Login");
-                        Utils.printLine(60);
-                    
-                        String retryChoice = "";
-                    
+                        Utils.displayHeader("Login");
                         System.out.print(" Do you want to try again? (Y/y or N/n): ");
-                        
-                        retryChoice = scanner.nextLine().trim().toLowerCase();  // Accept both uppercase and lowercase 'y'/'n'
+                        String retryChoice = "";
+                        retryChoice = scanner.nextLine().trim().toLowerCase();
 
                         if (retryChoice.equals("n")) {
-                            Utils.clearConsole();
-                            loginSuccess = true;  // Exit the login loop if user chooses 'n'
-                            Utils.printLine(60);
-                            Utils.printCentered("Login");
-                            Utils.printLine(60);
-                            Utils.printCentered("Exiting login.....");
-                            Utils.sleepFor(2000);
-                            Utils.clearConsole();
+                            loginSuccess = true;
+                            Utils.displayHeader("Login");
+                            Utils.displayMessage("Exiting login.....");
                             break;
                         } else if (retryChoice.equals("y")) {
-                            Utils.clearConsole();
-                            Utils.printLine(60);
-                            Utils.printCentered("Login");
-                            Utils.printLine(60);
-                            // Restart the login process by continuing the loop
-                            Utils.printCentered("Retrying login.....");
-                            Utils.sleepFor(2000);
-                            Utils.clearConsole();
-                            break;  // Break out of the loop to restart the login process
+                            Utils.displayHeader("Login");
+                            Utils.displayMessage("Retrying login.....");
+                            break;
                         } else {
-                            Utils.clearConsole();
-                            Utils.printLine(60);
-                            Utils.printCentered("Login");
-                            Utils.printLine(60);
-                            Utils.printCentered("Invalid input. Please enter 'Y/y' or 'N/n'.");
-                            Utils.sleepFor(1500);
-                            Utils.clearConsole();
-                            // The loop will continue and prompt again for a valid input
+                            Utils.displayHeader("Login");
+                            Utils.displayMessage("Invalid input, please enter 'Y/y' or 'N/n'.");
                         }
                     }
                 }
             }
         } else if (choice == 2) {
-            // Exit program
-            Utils.clearConsole();
-            Utils.printLine(60);
-            Utils.printCentered(" Main Menu");
-            Utils.printLine(60);
-            Utils.printCentered("Exiting... Goodbye!");
-            Utils.sleepFor(1500);
-            Utils.clearConsole();
+            Utils.displayHeader("Main Menu");
+            Utils.displayMessage("Exiting... Goodbye!");
             System.exit(0);
-        } else {
-            System.out.println("Invalid choice, please try again.");
         }
     }
+
+    private static boolean hasSeenGreeting = false;
 
     private static void showRoleBasedMenu() {
         User currentUser = UserManager.getCurrentUser();
         String role = currentUser.getRole();
-        Utils.printUserHeader(currentUser);
+
+        if (!hasSeenGreeting) {
+            Utils.printMenuGreeting(currentUser);
+            hasSeenGreeting = true;
+        }
+        
         if (role.equals("Admin")) {
             AdminMenu.showAdminMenu(scanner);
         } else if (role.equals("Employee")) {
             EmployeeMenu.showEmployeeMenu(scanner);
         }
+    }
+
+    public static void handleLogout() {
+        UserManager.logout();
+        hasSeenGreeting = false;
     }
 }
