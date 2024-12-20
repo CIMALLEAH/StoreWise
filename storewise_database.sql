88bfc4469a8f9ae06ead5af8fba2adfaab7c037d
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-12-20 21:54:29 +0800

    Update storewise_database.sql

--- storewise_database.sql
+++ storewise_database.sql
@@ -7,25 +7,25 @@ USE storewise;
 -- Create the `products` table
 CREATE TABLE `products` (
   `ProductID` int(11) NOT NULL AUTO_INCREMENT,
-  `ProductName` varchar(50) COLLATE NOT NULL,
-  `ProductGenCat` varchar(50) COLLATE NOT NULL,
-  `ProductSpecificCat` varchar(50) COLLATE NOT NULL,
+  `ProductName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ProductGenCat` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ProductSpecificCat` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `StockLevel` int(11) NOT NULL,
   `expirationDate` date DEFAULT NULL,
-  `AddedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Date&Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ProductID`),
-  UNIQUE KEY `AddedTime_UNIQUE` (`AddedTime`)
-);
+  UNIQUE KEY `AddedTime_UNIQUE` (`Date&Time`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Create the `equipments` table
 CREATE TABLE `equipments` (
   `equipmentID` int(11) NOT NULL AUTO_INCREMENT,
-  `equipmentName` varchar(255) COLLATE NOT NULL,
-  `equipmentCategory` varchar(255) COLLATE NOT NULL,
-  `equipmentStatus` varchar(50) COLLATE NOT NULL,
+  `equipmentName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `equipmentCategory` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `equipmentStatus` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `AddedTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`equipmentID`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Create the `maintenance_records` table
 CREATE TABLE `maintenance_records` (
@@ -38,7 +38,7 @@ CREATE TABLE `maintenance_records` (
   PRIMARY KEY (`recordID`),
   KEY `equipmentID` (`equipmentID`),
   CONSTRAINT `maintenance_records_ibfk_1` FOREIGN KEY (`equipmentID`) REFERENCES `equipments` (`equipmentID`) ON DELETE CASCADE
-);
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Create the `users` table
 CREATE TABLE `users` (
@@ -49,7 +49,7 @@ CREATE TABLE `users` (
   `CreationTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `UserName` (`UserName`)
-) 
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
   
 -- Insert sample data into 'products'
 INSERT INTO products (productName, productGenCat, productSpecificCat, stockLevel, expirationDate) VALUES
@@ -66,5 +66,10 @@ INSERT INTO equipments (equipmentName, equipmentCategory, equipmentStatus) VALUE
 INSERT INTO users (UserName, Password, UserRole) VALUES
 ('Will', '03221971', 'Admin'),
 ('Mike', '04071971', 'Employee'),
+
+-- Insert sample data into `maintenance_records`
+INSERT INTO maintenance_records (equipmentID, maintenanceDate, details, nextMaintenanceDate) VALUES
+('1','2024-12-20', 'Wiring', '2025-01-20');
+('2','2024-12-20', 'Engine', '2025-01-20');
 
 
