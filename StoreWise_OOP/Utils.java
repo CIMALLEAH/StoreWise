af0959cd4ff35f8dc83fe26cb27ffd365aacb8e9
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-12-20 21:48:05 +0800

    Update Utils.java

--- StoreWise_OOP/Utils.java
+++ StoreWise_OOP/Utils.java
@@ -5,15 +5,13 @@ import StoreWise_OOP.Manage_Users.User;
 public class Utils {
     private static final int CONSOLE_WIDTH = 60;
 
-    // Utility method to print a line of hyphens
     public static void printLine(int length) {
         for (int i = 0; i < length; i++) {
-            System.out.print("-");  // Print a hyphen for each iteration
+            System.out.print("-"); 
         }
-        System.out.println();  // Move to the next line after printing the hyphens
+        System.out.println(); 
     }
 
-    // Utility method to print text centered
     public static void printCentered(String text) {
         if (text != null) {
             int spacesToPad = (CONSOLE_WIDTH - text.length()) / 2;
@@ -22,15 +20,12 @@ public class Utils {
         }
     }
 
-    // Utility method to clear the console screen
     public static void clearConsole() {
         try {
             String operatingSystem = System.getProperty("os.name").toLowerCase();
             if (operatingSystem.contains("win")) {
-                // Windows
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
-                // Unix-based systems (Linux, macOS)
                 new ProcessBuilder("clear").inheritIO().start().waitFor();
             }
         } catch (Exception e) {
@@ -38,7 +33,6 @@ public class Utils {
         }
     }
 
-    // Utility method to pause for a certain amount of time (milliseconds)
     public static void sleepFor(int milliseconds) {
         try {
             Thread.sleep(milliseconds);
@@ -47,7 +41,6 @@ public class Utils {
         }
     }
 
-    // Utility method to print a personalized greeting message based on the current user
     public static void printMenuGreeting(User currentUser) {
         printLine(60);
         printCentered(currentUser.getRole() + " Menu");
@@ -57,7 +50,6 @@ public class Utils {
         clearConsole();
     }
 
-    // Utility method to display the menu header based on the user's role
     public static void displayUserHeader(User currentUser) {
         clearConsole();
         printLine(60);
